09e76b3e175d300065a6827ae45dbbe32583c389
Vinayak <workvinayak95@gmail.com>
2017-07-31 01:16:53 +0530

    fixed img issue

--- popup/page.js
+++ popup/page.js
@@ -3,9 +3,7 @@ function updateTab(tabs)
 	var myHeaders = new Headers();
 
 	var myInit = { method: 'GET',
-	               headers: myHeaders,
-	               mode: 'cors',
-	               cache: 'default' };
+	               headers: myHeaders };
 
 	var URL="https://www.saveitoffline.com/process/?url="+tabs[0].url+"&type=json";
 
@@ -15,27 +13,29 @@ function updateTab(tabs)
 	  return response.json();
 	}).then(function(data) {
 		document.getElementById("title").innerHTML=data['title'];
-		document.getElementById("icon").src=data['thumbnail'];
+		let imgLink=data['thumbnail'];
+
+		if(imgLink.indexOf("https")==-1)
+			document.getElementById("icon").src="https:"+imgLink;
+		else
+			document.getElementById("icon").src=imgLink;
+
 		var links=document.getElementById("links");
 		let x=0;
-		console.log(data['urls']);
 		while(1>0)
 		{
 			let val=data['urls'][x]
 			let label=val['label'];
-			console.log(label);
+			console.log(val['id']);
 			let consider=label.indexOf("video");
-			console.log(consider);
 			
 			if(consider!=-1)
 				break;
 			else
 			{
 				let a=document.createElement('a');
-				let linkText=document.createTextNode(label);
-				a.appendChild(linkText);
-				a.title=linkText;
-				a.href=val['id'];
+				a.setAttribute("href",val['id']);
+				a.innerHTML=val['label'];
 				links.appendChild(a);
 				x+=1	
 			}
@@ -46,6 +46,9 @@ function updateTab(tabs)
 }
 
 
+document.getElementsByTagName("a").onclick = function(e){
+	alert(e.href+" - "+this.href);
+}
 
 var gettingActiveTab = browser.tabs.query({active: true, currentWindow: true});
 gettingActiveTab.then(updateTab);
