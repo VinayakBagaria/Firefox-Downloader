7ee91061abea7075abff1a2af8d3160a08f67c41
Vinayak <workvinayak95@gmail.com>
2017-08-01 22:30:05 +0530

    for loop instead of while

--- popup/page.js
+++ popup/page.js
@@ -14,35 +14,44 @@ function updateTab(tabs)
 	fetch(myRequest).then(function(response) {
 	  return response.json();
 	}).then(function(data) {
-		document.getElementById("title").innerHTML=data['title'];
-		//document.getElementById("icon").src=data['thumbnail'];
-		var links=document.getElementById("links");
-		let x=0;
-		console.log(data['urls']);
-		while(1>0)
+		// if website doesnt contain a valid video file
+		if(data['error']=="no_media_found")
 		{
-			let val=data['urls'][x]
+			document.getElementById("title").innerHTML="No video link found";	
+			return;
+		}
+		// get title of video
+		document.getElementById("title").innerHTML=data['title'];
+		// get all links of video
+		let links=document.getElementById("links");
+		// display links
+		let x=0;
+		// get all urls
+		let urls=data['urls'];
+		for(x=0;x<urls.length;x++)
+		{
+			let val=urls[x]
+			
+			// get label value, identifying type of video
 			let label=val['label'];
-			console.log(label);
+
+			// if video word occurs in the label, then dont display further links
 			let consider=label.indexOf("video");
-			console.log(consider);
 			
 			if(consider!=-1)
-				break;
-			else
-			{
-				let a=document.createElement('a');
-				let linkText=document.createTextNode(label);
-				a.appendChild(linkText);
-				a.title=linkText;
-				//Only 360p video
-				if(label == "360p - mp4")
-				document.getElementById("vid").src=val['id'];	
-									
-				a.href=val['id'];
-				links.appendChild(a);
-				x+=1	
-			}
+				return;
+			
+			// create the a tag			
+			let a=document.createElement('a');
+			let linkText=document.createTextNode(label);
+			a.appendChild(linkText);
+			a.title=linkText;
+			a.href=val['id'];
+			links.appendChild(a);
+
+			//Only 360p video in the link
+			if(label == "360p - mp4")
+			document.getElementById("vid").src=val['id'];	
 		}
 
 	});
