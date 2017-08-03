5b0435824705d99846d4751a257cba392add3b60
Vinayak <workvinayak95@gmail.com>
2017-08-03 21:45:12 +0530

    fixed innerhtml

--- popup/page.js
+++ popup/page.js
@@ -1,29 +1,35 @@
 function updateTab(tabs)
 {
-	var myHeaders = new Headers();
+	let myHeaders = new Headers();
 
-	var myInit = { method: 'GET',
+	let myInit = { method: 'GET',
 	               headers: myHeaders,
 	               mode: 'cors',
 	               cache: 'default' };
 
-	var URL="https://www.saveitoffline.com/process/?url="+tabs[0].url+"&type=json";
+	let URL='https://www.saveitoffline.com/process/?url='+tabs[0].url+'&type=json';
 
-	var myRequest = new Request(URL, myInit);
+	let myRequest = new Request(URL, myInit);
 	
 	fetch(myRequest).then(function(response) {
 	  return response.json();
 	}).then(function(data) {
+		// get title parent h4 and make the title
+		let h4=document.createElement('h4');
+		h4.id='title';
+		
 		// if website doesnt contain a valid video file
 		if(data['error'])
 		{
-			document.getElementById("title").innerHTML="No video link found";	
+			h4.textContent='No video link found';	
+			document.getElementById('information').appendChild(h4);
 			return;
 		}
 		// get title of video
-		document.getElementById("title").innerHTML=data['title'];
+		h4.textContent=data['title'];
+		document.getElementById('information').appendChild(h4);
 		// get all links of video
-		let links=document.getElementById("links");
+		let links=document.getElementById('links');
 		// display links
 		let x=0;
 		// get all urls
@@ -36,7 +42,7 @@ function updateTab(tabs)
 			let label=val['label'];
 
 			// if video word occurs in the label, then dont display further links
-			let consider=label.indexOf("video");
+			let consider=label.indexOf('video');
 			
 			if(consider!=-1)
 				return;
@@ -47,12 +53,14 @@ function updateTab(tabs)
 			a.appendChild(linkText);
 			a.title=linkText;
 			a.href=val['id'];
-			a.id="video"
+			a.id='video'
 			links.appendChild(a);
 
 			//Only 360p video in the link
-			if(label == "360p - mp4")
-			document.getElementById("vid").src=val['id'];	
+			if(label == '360p - mp4')
+			{
+				document.getElementById('vid').src=val['id'];
+			}
 		}
 
 	});
@@ -61,7 +69,5 @@ function updateTab(tabs)
 
 
 
-var gettingActiveTab = browser.tabs.query({active: true, currentWindow: true});
+let gettingActiveTab = browser.tabs.query({active: true, currentWindow: true});
 gettingActiveTab.then(updateTab);
-
-
