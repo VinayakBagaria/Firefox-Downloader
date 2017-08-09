0c628d0549c3426e0189647bd5a5981ceb40dccb
Vinayak <workvinayak95@gmail.com>
2017-08-09 14:07:05 +0530

    linting for js file

--- popup/page.js
+++ popup/page.js
@@ -7,7 +7,7 @@ function updateTab(tabs)
 	               mode: 'cors',
 	               cache: 'default' };
 
-	let URL='https://www.saveitoffline.com/process/?url='+tabs[0].url+'&type=json';
+	let URL = 'https://www.saveitoffline.com/process/?url='+tabs[0].url+'&type=json';
 
 	let myRequest = new Request(URL, myInit);
 
@@ -15,56 +15,54 @@ function updateTab(tabs)
 	  return response.json();
 	}).then(function(data) {
 		// get title parent h4 and make the title
-		let h4=document.createElement('h4');
-		h4.id='title';
+		let h4 = document.createElement('h4');
+		h4.id = 'title';
 
 		// if website doesnt contain a valid video file
 		if(data['error'])
 		{
-			h4.textContent='No video link found';
+			h4.textContent = 'No video link found';
 			document.getElementById('information').appendChild(h4);
 			return;
 		}
 		// get title of video
-		h4.textContent=data['title'];
+		h4.textContent = data['title'];
 		document.getElementById('information').appendChild(h4);
 		// get all links of video
-		let links=document.getElementById('links');
+		let links = document.getElementById('links');
 		// display links
-		let x=0;
+		let x = 0;
 		// get all urls
-		let urls=data['urls'];
-		for(x=0;x<urls.length;x++)
+		let urls = data['urls'];
+		for(x = 0; x<urls.length; x++)
 		{
-			let val=urls[x]
+			let val = urls[x]
 
 			// get label value, identifying type of video
-			let label=val['label'];
+			let label = val['label'];
 
 			// if video word occurs in the label, then dont display further links
-			let consider=label.indexOf('video');
+			let consider = label.indexOf('video');
 
-			if(consider!=-1)
+			if(consider != -1)
 				return;
 
 			// create the a tag
-			let a=document.createElement('a');
-			let linkText=document.createTextNode(label);
+			let a = document.createElement('a');
+			let linkText = document.createTextNode(label);
 			a.appendChild(linkText);
-			a.title=linkText;
-			a.href=val['id'];
-			a.id='video'
+			a.title = linkText;
+			a.href = val['id'];
+			a.id = 'video'
 			links.appendChild(a);
 
 			//Only 360p video in the link
 			if(label == '360p - mp4')
 			{
-				document.getElementById('vid').src=val['id'];
+				document.getElementById('vid').src = val['id'];
 			}
 		}
-
 	});
-
 }
 
 
